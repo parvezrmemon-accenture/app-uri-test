5965638cb3b088984729f390163e25a27b59cae7
Parvez R Memon Accenture <parvez.r.memon.accenture@gmail.com>
2023-02-21 19:41:54 +0530

    added pwa support

--- sw.js
+++ sw.js
@@ -1,27 +1,63 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
-const urlsToCache = [
-  '/',
-  'index.html'
-];
+const staticCacheName = "site-static-v2";
+const dynamicCacheName = "site-dynamic-v1";
+const assets = ["/app-uri-test/", "/app-uri-test/index.html"];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then((cache) => {
+    cache.keys().then((keys) => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
+// install event
+self.addEventListener("install", (evt) => {
+  //console.log('service worker installed')
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
+// activate event
+self.addEventListener("activate", (evt) => {
+  //console.log('service worker activated')
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      //console.log(keys)
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
 
-        return fetch(event.request);
+// fetch event
+self.addEventListener("fetch", (evt) => {
+  //console.log('fetch event', evt)
+  evt.respondWith(
+    caches
+      .match(evt.request)
+      .then((cacheRes) => {
+        return (
+          cacheRes ||
+          fetch(evt.request).then((fetchRes) => {
+            return caches.open(dynamicCacheName).then((cache) => {
+              cache.put(evt.request.url, fetchRes.clone());
+              // check cached items size
+              limitCacheSize(dynamicCacheName, 15);
+              return fetchRes;
+            });
+          })
+        );
       })
+      .catch(() => {})
   );
 });
